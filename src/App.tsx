14d5ea19d2ed4986d41329f45dc29068e404b5b7
nitin pradhan <nitinpradhan48@gmail.com>
2026-04-12 22:25:32 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+ const App = () => {
+   return (
+    <>
+         <div id= "parent1">
+       <h1 id="child1">Hello this is child 1</h1>
+       <h2 id ="child2"> Hello this is child 2</h2>
+     </div>
+     <div id= "parent2">
+     <h1 id="child1">Hello this is child 1</h1>
+     <h2 id ="child2"> Hello this is child 2</h2>
+   </div>
+
+    </>
+   )
+ }
+ 
+ export default App
+ 
